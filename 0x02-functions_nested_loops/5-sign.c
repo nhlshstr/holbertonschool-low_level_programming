0fe7a44abafece4e698723d9aba1559d9b5bff5d
Nehal Shastri <nhlshstr@gmail.com>
2019-09-24 13:52:36 -0700

    changed wrong header

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/*
+ * print_sign - prints the sign of the given char
+ *
+ * Return: 1 if positive
+ *0 if zero
+ *-1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	return (0);
+}
