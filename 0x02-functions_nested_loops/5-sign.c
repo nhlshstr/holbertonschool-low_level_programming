5d72cd6fb2b607f4fe8e0f154b06e562bc139594
Nehal Shastri <nhlshstr@gmail.com>
2019-09-24 13:59:03 -0700

    removed bad line in comments

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
-/*
+/**
  * print_sign - prints the sign of the given char
  *
+ *@n: character being checked
  * Return: 1 if positive
  *0 if zero
  *-1 if negative
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
