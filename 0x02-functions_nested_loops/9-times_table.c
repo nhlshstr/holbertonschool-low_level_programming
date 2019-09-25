7f5ede12cfba99f4b1d8a2636242466bbe448a19
Nehal Shastri <nhlshstr@gmail.com>
2019-09-25 13:49:36 -0700

    changed parameters that were wrong for the formatting

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,22 +14,32 @@ void times_table(void)
 		{
 			pro = a * b;
 
-			if (pro < 10)
+			if (b == 0)
 			{
-				_putchar(pro + '0');
+				_putchar('0');
 				_putchar(',');
 				_putchar(' ');
 			}
+			else
+			{
 
-			if (pro > 10)
+			if (pro < 10)
+			{
+				_putchar(' ');
+			}
+
+			if (pro >= 10)
 			{
 				_putchar((pro / 10) + '0');
+			}	
+				
 				_putchar((pro % 10) + '0');
 				if (b != 9)
 				{
 				_putchar(',');
 				_putchar(' ');
 				}
+
 			}
 		}
 
