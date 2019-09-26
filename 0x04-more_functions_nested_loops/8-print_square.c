30845a0e5275b68141e4198b1e99fd650afafa03
Nehal Shastri <nhlshstr@gmail.com>
2019-09-26 13:37:58 -0700

    fixed for loop error

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *print_square - function to print square
+ *
+ *
+ * @size: defines the size of the square
+ *
+ * Return: no return type
+ */
+void print_square(int size)
+{
+	int c, d;
+
+	for (c = 0; c < size; c++)
+	{
+		for (d = 0; d < size; d++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
