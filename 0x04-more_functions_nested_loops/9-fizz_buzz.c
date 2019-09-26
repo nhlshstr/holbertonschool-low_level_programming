8a85418cc18da8c5952b5e1f4a4c2410a7df76c5
Nehal Shastri <nhlshstr@gmail.com>
2019-09-26 14:21:12 -0700

    removed space at the end of o/p

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,25 +11,32 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
+		if (a != 1)
+		{
+			printf(" ");
+		}
+
 		if (a % 5 == 0 && a % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 
 		else
 		{
-			printf("%d ", a);
+			printf("%d", a);
 		}
 	}
+
+	printf("\n");
 
 	return (0);
 }
