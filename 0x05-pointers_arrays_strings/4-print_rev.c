8fdb71e17df39edb74b68b32a0c80b4ddb19dcd3
Nehal Shastri <nhlshstr@gmail.com>
2019-10-01 15:32:28 -0700

    changed while loop logic

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *print_rev - function to print a string in reverse
+ *
+ *@s: string that is to be printed in reverse
+ *
+ *Return: No return type
+ */
+void print_rev(char *s)
+{
+	int count;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	while (count >= 0)
+	{
+		_putchar(*s);
+		s--;
+		count--;
+	}
+
+	_putchar('\n');
+}
