d99f8a336c1c737f7c203707ce0254c1f692e73c
Nehal Shastri <nhlshstr@gmail.com>
2019-10-02 01:50:07 -0700

    forgot to do a betty check

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
+ *print_array - function to print arrays
  *
+ *@a: array passed that is to be printed
  *
+ * @n: size of the array
  *
- *
+ * Return: No return for this function.
  */
 void print_array(int *a, int n)
 {
 	int i;
 	int newArr[5];
 
-	for(i = 0; i < n; i++)
-	{	
+	for (i = 0; i < n; i++)
+	{
 		if (i == (n - 1))
 		{
-		
+
 		newArr[i] = *(a + i);
 		printf("%d", newArr[i]);
 		break;
@@ -23,7 +26,7 @@ void print_array(int *a, int n)
 
 		newArr[i] = *(a + i);
 		printf("%d ,", newArr[i]);
-	}	
+	}
 
 	printf("\n");
 }
