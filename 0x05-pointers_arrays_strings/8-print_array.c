87415c933821040cae299c2c0bac5d6d66f93350
Nehal Shastri <nhlshstr@gmail.com>
2019-10-02 02:17:14 -0700

    tried new approach

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,21 +12,14 @@
 void print_array(int *a, int n)
 {
 	int i;
-	int newArr[5];
+
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == (n - 1))
+		printf("%d", a[i]);
+		if (i < (n - 1))
 		{
-
-		newArr[i] = *(a + i);
-		printf("%d", newArr[i]);
-		}
-
-		else
-		{
-		newArr[i] = *(a + i);
-		printf("%d, ", newArr[i]);
+			printf(", ");
 		}
 	}
 
