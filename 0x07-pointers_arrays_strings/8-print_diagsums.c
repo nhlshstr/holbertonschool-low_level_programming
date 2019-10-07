f9f39c7c15f3ee5692e6f694f38ac3c8b0edda88
Nehal Shastri <nhlshstr@gmail.com>
2019-10-07 16:38:05 -0700

    stopped traversing ahead and added next index in 2nd for loop

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,14 @@
 void print_diagsums(int *a, int size)
 {
 	int i, j;
-	int sum1, sum2;
+	int sum1 = 0, sum2 = 0;
 
 	for (i  = 0; i < size; i++)
-		sum1 += a[i * size + 1];
+		sum1 += *(a + (i * size) + i);
+
 	for (j = 0; j < size; j++)
-		sum2 += a[size - 1 + j * (size - 1)];
+		sum2 += *(a + (j * size) + (size - j - 1));
+
 	printf("%d, %d\n", sum1, sum2);
 
 }
