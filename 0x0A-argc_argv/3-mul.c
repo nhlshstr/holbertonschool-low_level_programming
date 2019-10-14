b54092a394d310e71e39318056547e9fcef13242
Nehal Shastri <nhlshstr@gmail.com>
2019-10-14 14:25:46 -0700

    added atoi()

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - multiplies 2 numbers
+ *
+ * @argc:  number of arguements
+ * @argv: stored arguements as strings
+ *
+ * Return: 1 if error, 0 otherwise.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 3)
+	{
+		a = atoi(*(argv + 1));
+		b = atoi(*(argv + 2));
+
+		printf("%d\n", (a * b));
+	}
+
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
+
