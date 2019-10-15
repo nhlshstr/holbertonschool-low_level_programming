6010642530c3451e86f8203d80ccae4f167c5d8d
Nehal Shastri <nhlshstr@gmail.com>
2019-10-15 16:42:13 -0700

    made old gcc compliant

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,11 +13,11 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
+	char *n = malloc(sizeof(char) * size);
+
 
 	if (size == 0)
 		return (NULL);
-
-	char *n = malloc(sizeof(char) * size);
 
 	if (n == NULL)
 	{
