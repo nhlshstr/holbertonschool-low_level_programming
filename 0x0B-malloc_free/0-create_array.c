cc5d9c8589c1909b40d4a45eba62328fb652eda4
Nehal Shastri <nhlshstr@gmail.com>
2019-10-15 15:25:10 -0700

    modified final return type to pointer

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *create_array - creates an array of chars, initializes it with a char
+ *
+ *@size: size of the mem allocated in bytes
+ *@c: character that is passed 
+ *
+ * Return: NULL if size = 0, NULL if fail.
+ * 		pointer to the array is returned, otherwise.
+ */
+char *create_array(unsigned int size, char c)
+{	
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	char *n = malloc(sizeof(char) * size);
+
+	if (n == NULL)
+	{
+		return (NULL);
+	}
+
+	for ( ; i < size; i++)
+	{
+		*(n + i) = c;
+	}
+
+	return (n);
+}
