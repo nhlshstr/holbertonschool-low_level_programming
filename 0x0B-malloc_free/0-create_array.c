93e66b70bc3e2d3dfd248594fb80838cb86791cd
Nehal Shastri <nhlshstr@gmail.com>
2019-10-15 15:28:21 -0700

    made betty compliant

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,13 +5,13 @@
  *create_array - creates an array of chars, initializes it with a char
  *
  *@size: size of the mem allocated in bytes
- *@c: character that is passed 
+ *@c: character that is passed
  *
  * Return: NULL if size = 0, NULL if fail.
- * 		pointer to the array is returned, otherwise.
+ *		pointer to the array is returned, otherwise.
  */
 char *create_array(unsigned int size, char c)
-{	
+{
 	unsigned int i = 0;
 
 	if (size == 0)
