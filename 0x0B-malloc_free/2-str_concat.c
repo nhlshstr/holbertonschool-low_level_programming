3dd6ed69523b57e9f80084cf37cf5ce5b690b9ad
Nehal Shastri <nhlshstr@gmail.com>
2019-10-15 20:54:13 -0700

    removed comment

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,11 +16,11 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 	char *p;
 	
-	/*if (s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
 	if(s1 == NULL)
 		s2 = "";
-	*/
+
 	for ( ; s1[i]; i++)
 	{
 		;
