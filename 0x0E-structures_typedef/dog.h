1b9df49eab9cefcec7d7a85f137c75d568f46eea
Nehal Shastri <nhlshstr@gmail.com>
2019-10-22 13:17:35 -0700

    more push

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,8 @@ struct dog
  */
 typedef struct dog dog_t;
 char *_strcpy(char *dest, char *src);
-int _strlen(char *s)
+int _strlen(char *s);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
