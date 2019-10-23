8bbca4b17e94151209d4a0b7091e8ba69b0bd32b
Nehal Shastri <nhlshstr@gmail.com>
2019-10-23 16:58:29 -0700

    made betty compliant

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,6 +2,12 @@
 /**
  *int_index - searches for int
  *
+ * @array: array that is passed
+ * @size: size of the array passed
+ * @cmp: pointer to function passed to it
+ *
+ * Return: position of int if found,
+ *		-1 otherwise
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -14,7 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (pos = 0; pos < size; pos++)
 	{
-		if(cmp(array[pos]))
+		if (cmp(array[pos]))
 			return (pos);
 	}
 
