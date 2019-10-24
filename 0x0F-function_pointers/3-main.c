1ec3d06ffea8d05b8e9d693ffc8a64570725a2e7
Nehal Shastri <nhlshstr@gmail.com>
2019-10-24 14:39:30 -0700

    first push

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+/**
+ *main - main driver function
+ *
+ * @argc: number of arguements passed
+ * @argv: pointers to arguements as strings
+ *
+ * Return: Error status if wrong, 0 o/w
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", (*get_op_func)(argv[2])(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+/**
+ *op_add - adds the two ints
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: Sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - subtracts two ints
+ *
+ * @a: first int
+ * @b: second string
+ *
+ * Return: Difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - calculates products
+ *
+ *@a: first int
+ *@b: second int
+ *
+ * Return: Product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - divides two numbers
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: Returns quotient
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - calculates remainder
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: Returns quotient
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
