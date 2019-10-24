b0636ed531b8c7520d10aea8b9a17a0090da2cbd
Nehal Shastri <nhlshstr@gmail.com>
2019-10-24 14:42:39 -0700

    fixed div case

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]))
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
