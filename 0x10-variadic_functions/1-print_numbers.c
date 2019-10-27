7abc2017746062507efbee50476e9bc3cc0b567e
nhlshstr <nhlshstr@gmail.com>
2019-10-27 10:42:43 -0700

    added NULL case for separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,12 +8,12 @@
  * Return: No return (void)
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
-{
+{	
 	unsigned int i = 0;
 	va_list args;
 
 	va_start(args, n);
-
+	
 	for (i = 0; i < n; i++)
 	{
 		if (i == (n - 1))
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d\n", va_arg(args, int));
 			break;
 		}
-		printf("%d%s", (va_arg(args, int)), separator);
+		printf("%d%s", (va_arg(args, int)), ((separator != NULL) ? separator : "" ));
 	}
 
 	va_end(args);
