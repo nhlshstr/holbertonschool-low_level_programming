cdedf4147f67744a4aa75a67517efae8cf9ca1c1
nhlshstr <nhlshstr@gmail.com>
2019-10-27 10:59:43 -0700

    modified main logic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,13 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == (n - 1))
-		{
-			printf("%d\n", va_arg(args, int));
-			break;
-		}
-		printf("%d%s", (va_arg(args, int)), ((separator != NULL) ? separator : ""));
-	}
+		printf("%d", va_arg(args, int));
 
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	
+	printf("\n");
 	va_end(args);
 }
