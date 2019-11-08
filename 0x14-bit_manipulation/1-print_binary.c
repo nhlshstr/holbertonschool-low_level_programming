05d7df3676cda7df09b02ad7a29f1a5e16e87f0a
nhlshstr <nhlshstr@gmail.com>
2019-11-08 10:37:51 -0800

    first one

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *print_binary - prints the binary version
+ *
+ * @n: number passed
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+	int print = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+
+	else
+	{
+		for (i = 1UL << 63; i > 0; i >>= 1)
+		{
+			if (n & i)
+			{
+				_putchar('1');
+				print = 1;
+			}
+			else if (print)
+				_putchar('0');
+		}
+	}
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,9 @@
 #ifndef HOLBESH
 #define HOLBESH
 #include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif 
