6faf29cc2274d19b69c732ae43c4f6581e49f586
nhlshstr <nhlshstr@gmail.com>
2019-11-08 13:45:06 -0800

    checked betty

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *flip_bits - counts the number of bits that need to be flipped
+ *
+ * @n: First number to be flipped
+ *@m: Second number to be flipped
+ *
+ * Return: Returns the number of bits to be flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int mask;
+	unsigned int a = m ^ n;
+	int count = 0;
+
+	for (mask = 1; mask != 0; mask << 1)
+	{
+		if (a & mask)
+			count += 1;
+	}
+
+	return (count);
+}
