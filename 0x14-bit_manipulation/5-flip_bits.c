e514708b480b08491193ea9a91aeb41a7627cce7
nhlshstr <nhlshstr@gmail.com>
2019-11-08 13:52:43 -0800

    checked betty

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int mask;
 	unsigned long int a = m ^ n;
 	int count = 0;
-	
+
 	for (mask = 1; mask != 0; mask <<= 1)
 	{
 		if (a & mask)
