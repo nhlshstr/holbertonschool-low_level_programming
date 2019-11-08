ec029a3db21a24df64b9df6eb4fb01aa524cb2fc
nhlshstr <nhlshstr@gmail.com>
2019-11-08 13:47:55 -0800

    changed for loop

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int a = m ^ n;
 	int count = 0;
 
-	for (mask = 1; mask != 0; mask << 1)
+	for (mask = 1; mask != 0; mask <<= 1)
 	{
 		if (a & mask)
 			count += 1;
