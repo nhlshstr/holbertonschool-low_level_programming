4fb1c123aa1c90f91a63dac5aca09ebb89d5dd4f
nhlshstr <nhlshstr@gmail.com>
2019-11-12 15:57:11 -0800

    changed write parameter

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (rd == -1)
 		return (0);
 
-	wrt = write(STDOUT_FILENO, pLetters, letters);
+	wrt = write(STDOUT_FILENO, pLetters, rd);
 	if (wrt == -1)
 		return (0);
 	close(fd);
