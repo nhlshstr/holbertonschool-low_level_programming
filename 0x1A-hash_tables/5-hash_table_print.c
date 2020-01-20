34b4a327735b2c6ea1490a2c675fd8dec9a4fc21
Nehal Shastri <nhlshstr@gmail.com>
2020-01-19 22:49:30 -0800

    betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -27,7 +27,7 @@ void hash_table_print(const hash_table_t *ht)
 	}
 
 	printf("{");
-	
+
 	for (i = 0; i < ht->size; i++)
 	{
 		tmp = ht->array[i];
@@ -43,6 +43,6 @@ void hash_table_print(const hash_table_t *ht)
 			}
 		}
 	}
-	
+
 	printf("}\n");
 }
