f0d655750acc899213d537d7dfa486210e169854
Nehal Shastri <nhlshstr@gmail.com>
2020-02-21 17:27:03 -0800

    did it

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,12 +8,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0 or None:
+                if i - 1 < 0 or grid[i - 1][j] == 0:
                     count += 1
-                if grid[i + 1][j] == 0 or None:
+                if i + 1 >= len(grid) or grid[i + 1][j] == 0:
                     count += 1
-                if grid[i][j - 1] == 0 or None:
+                if j - 1 < 0 or grid[i][j - 1] == 0:
                     count += 1
-                if grid[i][j + 1] == 0 or None:
+                if j + 1 >= len(grid[0]) or grid[i][j + 1] == 0:
                     count += 1
     return count
