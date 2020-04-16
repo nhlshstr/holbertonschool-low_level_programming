44f7b2c61f907beaa6e54555df29ffbfe5d6a162
Nehal Shastri <nhlshstr@gmail.com>
2020-04-15 19:42:17 -0700

    rewrote

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,40 +1,31 @@
 #include "search_algos.h"
 /**
- * jump_search - performs jump search
- * @array: Array that is passed
- * @size: size of the array
- * @value: value that is being searched
+ * jump_search - Performs jump search
+ * @array: Array passed to the function
+ * @size: Size of the array
+ * @value: Value looked for
  *
- * Return: Index of found value, -1 on error.
+ * Return: Index of found value, -1 o/w
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-	int optimal_jump = (int) floor(sqrt(size));
-	int cur = 0, j = 0, size1 = size;
-	size_t current = 0;
+	int step = (size_t) floor(sqrt(size)),
+	       cur = 0,
+	       j = 0;
 
 	if (!array)
 		return (-1);
 
-	for (cur = 0; value > array[cur]; cur += optimal_jump)
-	{
-		current = cur;
-		if (current > size)
-			break;
+	for (cur = 0; value > array[cur] && cur < (int) size; cur += step)
 		printf("Value checked array[%d] = [%d]\n", cur, array[cur]);
-	}
 
-	printf("Value found between indexes [%d] and [%d]\n", (
-				cur - optimal_jump), cur);
+	printf("Value found between indexes [%d] and [%d]\n", cur - step, cur);
 
-	for (j = cur - optimal_jump; j <= cur; j++)
+	for (j = cur - step; j < cur && j < (int) size; j++)
 	{
-		if (j >= size1)
-			break;
 		printf("Value checked array[%d] = [%d]\n", j, array[j]);
 		if (array[j] == value)
-			return (j);
+                        return (j);
 	}
 
 	return (-1);
