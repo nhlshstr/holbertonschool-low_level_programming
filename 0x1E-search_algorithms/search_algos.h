55f1cb58ebf6282399b4bf7e612616c5346c0392
Nehal Shastri <nhlshstr@gmail.com>
2020-04-15 20:00:58 -0700

    added stdlib

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
