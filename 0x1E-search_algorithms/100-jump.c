4592886ff0a7bb497ccf4a4334cc79d30e613c90
Nehal Shastri <nhlshstr@gmail.com>
2020-04-15 20:20:31 -0700

    added with betty

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -25,7 +25,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%d] = [%d]\n", j, array[j]);
 		if (array[j] == value)
-                        return (j);
+			return (j);
 	}
 
 	return (-1);
