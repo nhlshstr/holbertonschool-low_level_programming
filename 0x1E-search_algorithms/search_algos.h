4019a0994ab0c4a72822b3f7a9bb8aaeedeedd6d
Nehal Shastri <nhlshstr@gmail.com>
2020-04-15 17:10:46 -0700

    updated header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,8 @@
 #define HOLBY_SCHOOL
 #include <stddef.h>
 #include <stdio.h>
+#include <math.h>
 int linear_search(int *array, size_t size, int value);
-
-
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 #endif
