938aab7c0825511f49b6216155962d1f15bfb3af
Nehal Shastri <nhlshstr@gmail.com>
2020-04-17 00:14:00 -0700

    updated header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,8 +4,14 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+int binary_search_4exp(int *array, int value, int low, int high);
+int __min(int a, int b);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int rec_bs_helper(int *arr, int low, int high, int value, int result);
+void array_printer(int *pArray, int low, int high);
 int jump_search(int *array, size_t size, int value);
 #endif
